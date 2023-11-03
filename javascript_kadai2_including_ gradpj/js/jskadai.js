1844af2ca203042d70f1cd57e206d06c880d28b3
happyWeirdo <94735132+happyWeirdo@users.noreply.github.com>
2023-11-04 03:02:17 +0900

    Add files via upload
    README.mdはそれぞれのフォルダについています。
    ①ゲームアプリの写経
    ②卒業制作ver0.0.0
    ご確認よろしくお願いいたします。

---
+++ javascript_kadai2_including_ gradpj/js/jskadai.js
@@ -0,0 +1,209 @@
+    // const element = document.querySelector(".test");
+    // let t = 0; // 速度
+    // let freqX = getRandomFrequency();
+    // let freqY = getRandomFrequency();
+
+    // function getRandomFrequency() {
+    //     return 1 + Math.random() * 2; // 1〜3のランダムな数値を返す
+    // }
+
+    // function updatePosition() {
+    //     console.log(window.innerWidth);
+    //     console.log(window.innerHeight);
+    //     // 画面の幅・高さの半分を計算
+    //     const halfWidth = window.innerWidth / 2 - 25; // 要素の半分のサイズ
+    //     const halfHeight = window.innerHeight / 2 - 25; // 要素の半分のサイズ
+    //     // 正弦波を利用してX、Y座標を計算
+    //     const x = halfWidth * Math.sin(freqX * t);
+    //     const y = halfHeight * Math.sin(freqY * t);
+    //     // 壁に衝突した場合の処理
+    //     if (x >= halfWidth || x <= -halfWidth) {
+    //         freqX = getRandomFrequency() * (freqX > 0 ? -1 : 1); // Xの周波数をランダムに変更
+    //     }
+    //     if (y >= halfHeight || y <= -halfHeight) {
+    //         freqY = getRandomFrequency() * (freqY > 0 ? -1 : 1); // Yの周波数をランダムに変更
+    //     }
+    //     // 計算したX、Y座標を要素に適用
+    //     element.style.transform = `translate(${x}px, ${y}px)`;
+    //     t += 0.005; // 時間の加算（速度）
+    //     requestAnimationFrame(updatePosition); // 次のフレームで再度実行
+    // }
+    // updatePosition();
+    //jQueryでやることがなければ、vanilla jsでOK 
+    document.querySelector(".square0").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square1").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+
+    document.querySelector(".square2").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square3").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square4").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square5").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square6").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square7").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square8").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square9").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square10").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square11").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square12").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square13").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square14").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
+    document.querySelector(".square15").animate({
+        borderRadius: [
+            "50% 50% 50% 70%/50% 50% 70% 60%",
+            "80% 30% 50% 50%/50%",
+            "40% 40% 50% 40%/30% 50% 40% 80%"
+        ]
+    }, {
+        iterations: Infinity,
+        direction: "alternate",
+        duration: 7000
+    });
